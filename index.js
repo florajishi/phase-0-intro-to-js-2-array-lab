51f2eb067f7a4624d46a5bf2b85c03990d87088e
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2022-01-09 03:12:17 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+    const copyCat = [...cats, "Broom"]
+    return copyCat;
+}
+function prependCat(name) {
+    const copyCat = ["Arnold", ...cats]
+    return copyCat;
+}
+function removeLastCat() {
+    const copyCat = cats.slice(0, cats.length - 1)
+    return copyCat;
+}
+function removeFirstCat() {
+    const copyCat = cats.slice(1)
+    return copyCat;
+}
